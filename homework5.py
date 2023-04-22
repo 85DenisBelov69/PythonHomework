7325d8037d90fbff2cdfcb11d77b14494dadbd94
DenisBelov <olgabelova@192.168.0.2>
2023-04-22 13:50:45 +0700

    add homework5

---
+++ homework5.py
@@ -0,0 +1,13 @@
+#Задача 1. Создайте список. Запишите в него N первых элементов 
+# последовательности Фибоначчи.
+# 6 –> 1 1 2 3 5 8
+
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+list_1 = []
+for i in range(1,7):
+    list_1.append(fib(i))
+print(list_1)
